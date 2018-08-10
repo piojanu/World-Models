4930315f75108d31326dd671207f20ff0bef90de
Piotr Januszewski <piojanuszewski@gmail.com>
2018-08-10 17:36:23 +0200

    Finish TorchTrainer wrapper on torch.nn.Module

--- torchtrainer.py
+++ torchtrainer.py
@@ -1,3 +1,12 @@
+"""The MIT License
+Copyright 2018 Piotr Januszewski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."""
+
 import os.path
 
 import torch
@@ -5,7 +14,6 @@ import torch.nn as nn
 import torch.utils.data
 
 from collections import defaultdict
-from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
 
@@ -44,6 +52,39 @@ class Callback(object):
         pass
 
 
+class CallbackList(Callback):
+    """Simplifies calling all callbacks from list."""
+
+    def __init__(self, callbacks, trainer):
+        self.callbacks = callbacks
+        for callback in self.callbacks:
+            callback.trainer = trainer
+
+    def on_train_begin(self):
+        for callback in self.callbacks:
+            callback.on_train_begin()
+
+    def on_train_end(self, is_aborted):
+        for callback in self.callbacks:
+            callback.on_train_end(is_aborted)
+
+    def on_epoch_begin(self, epoch):
+        for callback in self.callbacks:
+            callback.on_epoch_begin(epoch)
+
+    def on_epoch_end(self, epoch, metrics):
+        for callback in self.callbacks:
+            callback.on_epoch_end(epoch, metrics)
+
+    def on_batch_begin(self, batch):
+        for callback in self.callbacks:
+            callback.on_batch_begin(batch)
+
+    def on_batch_end(self, batch, metrics):
+        for callback in self.callbacks:
+            callback.on_batch_end(batch, metrics)
+
+
 class EarlyStopping(Callback):
     """Stops training if given metrics doesn't improve."""
 
@@ -68,22 +109,22 @@ class EarlyStopping(Callback):
 
     def on_epoch_end(self, epoch, metrics):
         if not self.metric in metrics:
-            raise ValueError("There is no such metric evaluated: {}".format(self.metric))
+            raise ValueError("\tThere is no such metric evaluated: {}".format(self.metric))
 
         new_value = metrics[self.metric]
         if new_value < self._best_value:
             if self.verbose:
-                print("Metric '{}' improved from {} to {}".format(
+                print("\tMetric '{}' improved from {} to {}".format(
                     self.metric, self._best_value, new_value))
             self._best_value = new_value
             self._last_epoch = epoch
         elif self.verbose:
-            print("Metric '{}' did not improved. Current best: {}".format(
+            print("\tMetric '{}' did not improved. Current best: {}".format(
                 self.metric, self._best_value))
 
-        if self._last_epoch + self.patience >= epoch:
+        if self._last_epoch + self.patience <= epoch:
             if self.verbose:
-                print("Metric '{}' did not improved for {} epochs. Early stop!".format(
+                print("\tMetric '{}' did not improved for {} epochs. Early stop!".format(
                     self.metric, self.patience))
             self.trainer.early_stop()
 
@@ -131,50 +172,17 @@ class ModelCheckpoint(Callback):
         if not self.save_best:
             self.trainer.save_ckpt(self.path)
             if self.verbose:
-                print("Saving module state at: {}".format(self.path))
+                print("\tSaving module state at: {}".format(self.path))
         else:
             if not self.metric in metrics:
-                raise ValueError("There is no such metric evaluated: {}".format(self.metric))
+                raise ValueError("\tThere is no such metric evaluated: {}".format(self.metric))
 
             new_value = metrics[self.metric]
             if new_value < self._best_value:
                 if self.verbose:
-                    print("Saving new best module state at: {}".format(self.path))
+                    print("\tSaving new best module state at: {}".format(self.path))
                 self._best_value = new_value
                 self.trainer.save_ckpt(self.path)
-
-
-class CallbackList(Callback):
-    """Simplifies calling all callbacks from list."""
-
-    def __init__(self, callbacks, trainer):
-        self.callbacks = callbacks
-        for callback in self.callbacks:
-            callback.trainer = trainer
-
-    def on_train_begin(self):
-        for callback in self.callbacks:
-            callback.on_train_begin()
-
-    def on_train_end(self, is_aborted):
-        for callback in self.callbacks:
-            callback.on_train_end(is_aborted)
-
-    def on_epoch_begin(self, epoch):
-        for callback in self.callbacks:
-            callback.on_epoch_begin(epoch)
-
-    def on_epoch_end(self, epoch, metrics):
-        for callback in self.callbacks:
-            callback.on_epoch_end(epoch, metrics)
-
-    def on_batch_begin(self, batch):
-        for callback in self.callbacks:
-            callback.on_batch_begin(batch)
-
-    def on_batch_end(self, batch, metrics):
-        for callback in self.callbacks:
-            callback.on_batch_end(batch, metrics)
 
 
 class TorchTrainer(object):
@@ -185,6 +193,8 @@ class TorchTrainer(object):
 
         Args:
             model (torch.nn.Module): PyTorch neural net module.
+            DataTensor (torch.Tensor): PyTorch tensor type to use as data type.
+            TargetTensor (torch.Tensor): PyTorch tensor type to use as target type.
         """
 
         if not isinstance(model, nn.Module):
@@ -200,16 +210,17 @@ class TorchTrainer(object):
 
         Args:
             optimizer (torch.optim.Optimizer): Parameters optimizer.
-            loss (func): Loss function with signature `func(pred, target) -> scalar`, where
-                `pred` is PyTorch module output and `target` is target labels/values.
-            metrics (dict): List of functions with the same signature as loss. Those metrics will be
+            loss (func): Loss function with signature `func(pred, target) -> scalar tensor`, where
+                `pred` is PyTorch module output (can be tuple if multiple outputs) and `target`
+                is target labels/values from provided data (can be tuple if multiple targets).
+            metrics (list): List of functions with the same signature as loss. Those metrics will be
                 evaluated on each training and validation batch. Final value is always average
                 over all batches. (Default: always loss)
         """
 
         self.optim = optimizer
         self.loss = loss
-        self.metrics = {'loss': loss}
+        self.metrics = {}
         if metrics is not None:
             for metric in metrics:
                 self.metrics[metric.__name__] = metric
@@ -220,8 +231,12 @@ class TorchTrainer(object):
         """Evaluate PyTorch module on dataset.
 
         Args:
-            data (np.ndarray): Data to train on. First dimension must be batch dimension.
-            target (np.ndarray): Targets to train on. First dimension must be batch dimension.
+            data (torch.Tensor or list): Data to evaluate on or list of multiple data tensors.
+                Tensor shape should be 'N x *' where 'N' is number of examples and '*' indicates
+                any number of dimensions.
+            target (torch.Tensor or list): Target to evaluate on or list of multiple target tensors.
+                Tensor shape should be 'N x *' where 'N' is number of examples and '*' indicates
+                any number of dimensions.
             batch_size (int): Single update data batch size. (Default: 64)
             verbose (int): Two levels of verbosity:
                 * 1 - show progress bar,
@@ -230,18 +245,18 @@ class TorchTrainer(object):
 
         # Create data loader
         data_loader = torch.utils.data.DataLoader(
-            torch.utils.data.TensorDataset(torch.from_numpy(data), torch.from_numpy(target)),
+            torch.utils.data.TensorDataset(data, target),
             batch_size=batch_size,
             shuffle=False
         )
 
-        self.evaluate_loader(data_loader=data_loader, verbose=verbose)
+        return self.evaluate_loader(data_loader=data_loader, verbose=verbose)
 
     def evaluate_loader(self, data_loader, verbose=0):
         """Evaluate PyTorch module on dataset (using PyTorch DataLoader class).
 
         Args:
-            data_loader (torch.utils.data.DataLoader): Train data loader.
+            data_loader (torch.utils.data.DataLoader): Evaluation data loader.
             verbose (int): Two levels of verbosity:
                 * 1 - show progress bar,
                 * 0 - show nothing. (<- Default)
@@ -257,7 +272,7 @@ class TorchTrainer(object):
         results_avg = defaultdict(float)
         for iter_t, (data, target) in enumerate(
                 tqdm(data_loader, ascii=True, desc="Evaluate", disable=(not verbose))):
-            results_tmp = self._eval_metrics(data, target)
+            results_tmp, _ = self._eval_metrics(data, target)
             self._average_metrics(results_avg, results_tmp, iter_t)
 
         return results_avg
@@ -267,15 +282,21 @@ class TorchTrainer(object):
         """Fit PyTorch module to dataset.
 
         Args:
-            data (np.ndarray): Data to train on. First dimension must be batch dimension.
-            target (np.ndarray): Targets to train on. First dimension must be batch dimension.
+            data (torch.Tensor or list): Data to train on or list of multiple data tensors.
+                Tensor shape should be 'N x *' where 'N' is number of examples and '*' indicates
+                any number of dimensions.
+            target (torch.Tensor or list): Target to train on or list of multiple target tensors.
+                Tensor shape should be 'N x *' where 'N' is number of examples and '*' indicates
+                any number of dimensions.
             batch_size (int): Single update data batch size. (Default: 64)
             epochs (int): How many times iterate over whole dataset. (Default: 1)
             verbose (int): Two levels of verbosity:
                 * 1 - show progress bar, (<- Default)
                 * 0 - show nothing.
             callbacks (list of Callback): Event train, epoch and batch events listeners.
-            validation_split (float): How big part of data put away for validation. (Default: 0.0)
+            validation_split (float): How big part of data put away for validation. The validation
+                data is selected from the last samples in the `data` and `target` provided,
+                before shuffling (Default: 0.0)
             validation_data (tuple): Tuple: (val_data, val_targets). Overwrites `validation_split`.
             shuffle (bool): If randomize data order in each epoch. (Default: True)
             initial_epoch (int): From which number start to count epochs. (Default: 0)
@@ -286,15 +307,16 @@ class TorchTrainer(object):
             X_train, y_train = data, target
             X_val, y_val = validation_data
         elif validation_split > 0.0 and validation_split < 1.0:
-            X_train, X_val, y_train, y_val = train_test_split(
-                data, target, test_size=validation_split)
+            split = int(data.shape[0] * validation_split)
+            X_train, y_train = data[:split], target[:split]
+            X_val, y_val = data[split:], target[split:]
         else:
             X_train, y_train = data, target
             X_val, y_val = None, None
 
         # Create training data loader
         data_loader = torch.utils.data.DataLoader(
-            torch.utils.data.TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train)),
+            torch.utils.data.TensorDataset(X_train, y_train),
             batch_size=batch_size,
             shuffle=shuffle
         )
@@ -303,7 +325,7 @@ class TorchTrainer(object):
         validation_loader = None
         if X_val is not None and y_val is not None:
             validation_loader = torch.utils.data.DataLoader(
-                torch.utils.data.TensorDataset(torch.from_numpy(X_val), torch.from_numpy(y_val)),
+                torch.utils.data.TensorDataset(X_val, y_val),
                 batch_size=batch_size,
                 shuffle=False
             )
@@ -341,26 +363,26 @@ class TorchTrainer(object):
 
                 # Train on whole dataset
                 results_avg = defaultdict(float)
-                pbar = tqdm(data_loader, ascii=True,
-                            desc="Epoch: {:2d}/{}".format(epoch + 1, epochs), disable=(not verbose))
-                for iter_t, (data, target) in enumerate(pbar):
-                    callbacks_list.on_batch_begin(iter_t)
+                with tqdm(data_loader, ascii=True, desc="{:2d}/{}".format(epoch + 1, epochs),
+                          disable=(not verbose)) as pbar:
+                    for iter_t, (data, target) in enumerate(pbar):
+                        callbacks_list.on_batch_begin(iter_t)
 
-                    self.optim.zero_grad()
+                        self.optim.zero_grad()
 
-                    results_tmp = self._eval_metrics(data, target)
-                    self._average_metrics(results_avg, results_tmp, iter_t)
+                        results_tmp, loss = self._eval_metrics(data, target)
+                        self._average_metrics(results_avg, results_tmp, iter_t)
 
-                    results_tmp['loss'].backward()
-                    self.optim.step()
+                        loss.backward()
+                        self.optim.step()
 
-                    pbar.set_postfix(**results_avg)
-                    callbacks_list.on_batch_end(iter_t, results_tmp)
+                        callbacks_list.on_batch_end(iter_t, results_tmp)
 
-                if validation_loader is not None:
-                    results_val = self.evaluate_loader(validation_loader)
-                    results_avg = self._merge_results(results_avg, results_val)
-                    pbar.set_postfix(**results_avg)
+                        if (iter_t + 1) == len(data_loader) and validation_loader is not None:
+                            results_val = self.evaluate_loader(validation_loader)
+                            results_avg = self._merge_results(results_avg, results_val)
+
+                        pbar.set_postfix(results_avg)
 
                 callbacks_list.on_epoch_end(epoch, results_avg)
                 if self._early_stop:
@@ -420,13 +442,17 @@ class TorchTrainer(object):
 
         Return:
             dict: Metrics evaluation results, keys are metrics' names.
+            torch.Tensor: Module loss on given batch.
         """
 
-        results = {}
-        for key, func in self.metrics.items():
-            results[key] = func(self.model(data), target)
+        pred = self.model(data)
+        loss = self.loss(pred, target)
 
-        return results
+        results = {'loss': loss.item()}
+        for key, func in self.metrics.items():
+            results[key] = func(pred, target).item()
+            
+        return results, loss
 
     def _merge_results(self, train_r, val_r):
         """Merge training and validation metrics evaluation results.
@@ -453,3 +479,69 @@ class TorchTrainer(object):
         self._early_stop = False
         if not self._is_compiled:
             raise ValueError("You need to compile Trainer before using it!")
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import torch.optim as optim
+
+    from sklearn.cross_validation import train_test_split
+    from sklearn.datasets import load_digits
+
+    NUM_CLASSES = 10
+
+    # Load data and divide into datasets
+    data, target = load_digits(n_class=NUM_CLASSES, return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, target, test_size=0.2, stratify=target)
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, stratify=y_train)
+
+    # Build PyTorch module
+    class Net(nn.Module):
+        def __init__(self, input_size, num_classes, hidden_units=100):
+            super(Net, self).__init__()
+
+            self.h = nn.Linear(input_size, hidden_units)
+            self.out = nn.Linear(hidden_units, num_classes)
+
+            # Weights initialization
+            for m in self.modules():
+                if isinstance(m, nn.Linear):
+                    # 'n' is number of inputs to each neuron
+                    n = len(m.weight.data[1])
+                    # "Xavier" initialization
+                    m.weight.data.normal_(0, np.sqrt(2. / n))
+                    m.bias.data.zero_()
+
+        def forward(self, x):
+            hidden = torch.relu(self.h(x))
+            return self.out(hidden)
+
+    # Instantiate Torch Trainer!
+    net = TorchTrainer(Net(data.shape[1], NUM_CLASSES))
+
+    # Compile trainer
+    def acc(pred, target):
+        return torch.mean((torch.max(pred, 1)[1] == target).type(torch.FloatTensor))
+
+    net.compile(
+        optimizer=optim.SGD(net.model.parameters(), lr=1e-3, momentum=0.9),
+        loss=nn.CrossEntropyLoss(),
+        metrics=[acc]
+    )
+
+    # Fit module to training data
+    net.fit(
+        torch.FloatTensor(X_train), torch.LongTensor(y_train),
+        epochs=100,
+        validation_data=(torch.FloatTensor(X_val), torch.LongTensor(y_val)),
+        callbacks=[
+            EarlyStopping(verbose=1),
+            ModelCheckpoint("/tmp/best_digits.ckpt", save_best=True, verbose=1)
+        ]
+    )
+
+    # Evaluate module on test data
+    metrics = net.evaluate(torch.FloatTensor(X_test), torch.LongTensor(y_test), verbose=1)
+    print("Final weights loss: {}, accuracy: {}".format(metrics['loss'], metrics['acc']))
